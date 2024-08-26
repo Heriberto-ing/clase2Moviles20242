00059967c16a32f96e40e3a9b32f20843fa989a4
Heriberto-ing <142918003+Heriberto-ing@users.noreply.github.com>
2024-08-25 21:28:59 -0500

    nn

--- app/src/main/java/com/example/clase2/MainActivity.java
+++ app/src/main/java/com/example/clase2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.clase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.nfc.Tag;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,11 +17,13 @@ public class MainActivity extends AppCompatActivity {
     TextView formulario;
     Button guardar, borrar;
     String nombreUsuario, apeUsuario, edadUsuario, ciudadUsuario, barrioUsuario;
+    String TAG = "Test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Estoy en el onCreate");
 
         ingresarNombre = (EditText) findViewById(R.id.editarNombre);
         ingresarApe = (EditText) findViewById(R.id.editarApe);
@@ -28,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         formulario = (TextView) findViewById(R.id.txvFormulario);
         guardar = (Button) findViewById(R.id.btnGuardar);
         borrar = (Button) findViewById(R.id.btnBorrar);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Estoy en el onStart");
 
         guardar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +66,35 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Estoy en el onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Estoy en el onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Estoy en el onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Estoy en el onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Estoy en el onDestroy");
     }
 }
