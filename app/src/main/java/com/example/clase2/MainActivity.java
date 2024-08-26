a4fb0da766416030c124e82e6d50fc842ebc0c36
Heriberto-ing <142918003+Heriberto-ing@users.noreply.github.com>
2024-08-25 23:47:49 -0500

    nn

--- app/src/main/java/com/example/clase2/MainActivity.java
+++ app/src/main/java/com/example/clase2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.clase2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.nfc.Tag;
 import android.util.Log;
@@ -10,20 +11,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     EditText ingresarNombre, ingresarApe, ingresarEdad, ingresasCiudad, ingresarBarrio;
-    TextView formulario;
+    TextView formulario, estadoCV;
     Button guardar, borrar;
-    String nombreUsuario, apeUsuario, edadUsuario, ciudadUsuario, barrioUsuario;
-    String TAG = "Test";
+    String nombreUsuario, apeUsuario, edadUsuario, ciudadUsuario, barrioUsuario, EstadoTexto;
+    String EstadoAnterior = "None";
+    //String TAG = "Test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "Estoy en el onCreate");
 
         ingresarNombre = (EditText) findViewById(R.id.editarNombre);
         ingresarApe = (EditText) findViewById(R.id.editarApe);
@@ -33,13 +36,18 @@ public class MainActivity extends AppCompatActivity {
         formulario = (TextView) findViewById(R.id.txvFormulario);
         guardar = (Button) findViewById(R.id.btnGuardar);
         borrar = (Button) findViewById(R.id.btnBorrar);
+        estadoCV = (TextView) findViewById(R.id.estadoCV);
+
+        actualizarEstado("onCreate");
+        Log.d("Ciclo de vida", "Estoy en el onCreate");
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "Estoy en el onStart");
+        actualizarEstado("onStart");
+        Log.d("Ciclo de vida", "Estoy en el onStart");
 
         guardar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
                 edadUsuario = String.valueOf(ingresarEdad.getText());
                 ciudadUsuario = String.valueOf(ingresasCiudad.getText());
                 barrioUsuario = String.valueOf(ingresarBarrio.getText());
-
+                guardarDatos(nombreUsuario);
+                guardarDatos(apeUsuario);
+                guardarDatos(edadUsuario);
+                guardarDatos(ciudadUsuario);
+                guardarDatos(barrioUsuario);
             }
         });
 
@@ -71,30 +83,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "Estoy en el onResume");
+        actualizarEstado("onResume");
+        Log.d("Ciclo de vida", "Estoy en el onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "Estoy en el onPause");
+        actualizarEstado("onPause");
+        Log.d("Ciclo de vida", "Estoy en el onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "Estoy en el onStop");
+        actualizarEstado("onStop");
+        Log.d("Ciclo de vida", "Estoy en el onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "Estoy en el onRestart");
+        actualizarEstado("onRestart");
+        Log.d("Ciclo de vida", "Estoy en el onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "Estoy en el onDestroy");
+        actualizarEstado("onDestroy");
+        Log.d("Ciclo de vida", "Estoy en el onDestroy");
+    }
+
+    private void actualizarEstado(String EstadoActual) {
+        EstadoTexto = "Estado anterior: " + EstadoAnterior + "\nEstado actual: " + EstadoActual;
+        estadoCV.setText(EstadoTexto);
+        EstadoAnterior = EstadoActual;
+    }
+
+    private void guardarDatos(String datos) {
+        SharedPreferences sharedPreferences = getSharedPreferences("clase 2", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("datos Guardados", datos);
+        editor.apply();
+
+        Toast.makeText(this, "Datos guardados", Toast.LENGTH_SHORT).show();
     }
 }
